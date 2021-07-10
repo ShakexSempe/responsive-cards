e45ab1e279902f28769ead0fc4b6cef9ed8e0309
Ray-Shakex <bshakex@gmail.com>
2021-07-10 14:08:36 +0200

    main.js scroll function

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const navLinks = document.querySelectorAll('.nav-link') ;
 navLinks.forEach(link => {
     link.addEventListener("click", e => {
         e.preventDefault();
-        
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        console.log(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        position = element.offsetTop - navHeight;
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
     })
 })
