5a031874ba3c6690c934013eb61a3b6fe3e92226
Ray-Shakex <bshakex@gmail.com>
2023-10-13 12:54:00 +0200

    cards scss

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ const navbar = document.getElementById('navbar');
 const navHeight = navbar.getBoundingClientRect().height;
 const navLinks = document.querySelectorAll('.nav-link') ;
 
-navLinks.forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        const id = e.currentTarget.getAttribute('href').slice(1);
-        const element = document.getElementById(id);
-        console.log(id);
-        const navHeight = navbar.getBoundingClientRect().height;
-        position = element.offsetTop - navHeight;
+// navLinks.forEach(link => {
+//     link.addEventListener("click", e => {
+//         e.preventDefault();
+//         const id = e.currentTarget.getAttribute('href').slice(1);
+//         const element = document.getElementById(id);
+//         console.log(id);
+//         const navHeight = navbar.getBoundingClientRect().height;
+//         position = element.offsetTop - navHeight;
 
-        window.scrollTo({
-            left: 0,
-            top: position,
-        });
-    })
-})
+//         window.scrollTo({
+//             left: 0,
+//             top: position,
+//         });
+//     })
+// })
